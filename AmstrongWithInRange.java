3ee2eeffe8c305c8ef21fd92e5d9b64b24a4bccd
SHIVAWD93 <alamandashivakumar9090@gmail.com>
2021-11-25 10:43:21 +0530

    Modified-25/11/21

--- AmstrongWithInRange.java
+++ AmstrongWithInRange.java
@@ -1,10 +1,28 @@
 package com.technojava1;
 
+import java.util.Scanner;
+
 public class AmstrongWithInRange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter starting value");
+		int start = sc.nextInt();
+		System.out.println("enter ending value");
+		int end = sc.nextInt();
+		for (int i = start; i <= end; i++) {
+			int original = i;
+			int n = i;
+			int sum= 0;
+            while (n > 0) {
+				int temp = n % 10;
+				n = n / 10;
+				sum = sum + (temp * temp * temp);
+			}
+			if (sum == original) {
+				System.out.println(sum);
+				System.out.println("given number is amstrong");
+			}
+		}
 	}
-
 }
